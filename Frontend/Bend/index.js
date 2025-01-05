d465646306e011a68633c8b96f6152c91e55c588
Dharamkar Rohith Kumar <159691874+rohithkumardharamkar@users.noreply.github.com>
2025-01-05 13:55:30 +0530

    5

--- Frontend/Bend/index.js
+++ Frontend/Bend/index.js
@@ -7,12 +7,12 @@ require('dotenv').config()
 var bodyParser = require('body-parser');
 const userRoute = require("./routes/user.route");
 let app=express()
-mongoose.connect("mongodb://127.0.0.1:27017/Social").then((res)=>{ console.log("database connected");}).catch((err)=>{console.log(err);})
+mongoose.connect(process.env.mongo_url).then((res)=>{ console.log("database connected");}).catch((err)=>{console.log(err);})
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use("/imgs",express.static("./imgs"))
-let port= 5500
+let port= process.env.port || 5500
 
 
 app.use("/user",userRoute)
